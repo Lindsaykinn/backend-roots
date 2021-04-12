7c41597723b91925f43bba9d494a82c719e92492
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-11 22:10:40 -0400

    updated family table

--- db/migrate/20210403222445_create_families.rb
+++ db/migrate/20210403222445_create_families.rb
@@ -3,7 +3,10 @@ class CreateFamilies < ActiveRecord::Migration[6.1]
     create_table :families do |t|
       t.string :surname
       t.text :story
-      t.string :country_of_origin
+      t.string :country
+      t.string :person
+      t.date :dob
+
 
       t.timestamps
     end
